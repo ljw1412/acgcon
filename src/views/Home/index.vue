97737f9b82cad7ba00cb0a07f42a001d17cc0977
ljw1412 <506644791@qq.com>
2020-02-10 20:01:17 +0800

    首页背景请求

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="home">
-    <div class=""></div>
+    <div class="bg"
+      :style="{'background-image':`url('${backgroundUrl}')`}"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  backgroundUrl = ''
+
+  async fetchBackground() {
+    const imageList = (await this.$axios.get('base/bg_bing')).data
+    this.backgroundUrl = imageList[0].url
+  }
+
+  mounted() {
+    this.fetchBackground()
+  }
+}
 </script>
 
 <style lang="scss">
 .home {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -10;
-  background-color: red;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
+  .bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #00a1d6;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    z-index: -10;
+  }
 }
 </style>
