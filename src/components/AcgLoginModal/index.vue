8371372e975a552fea6003a06b6b4e67c6691ea4
ljw1412 <506644791@qq.com>
2020-03-24 18:57:25 +0800

    增加登录弹窗

---
+++ src/components/AcgLoginModal/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <mz-modal class="acg-login-modal"
+    v-model="mVisible">
+  </mz-modal>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Model } from 'vue-property-decorator'
+
+@Component
+export default class AcgLoginModal extends Vue {
+  @Model('input', { type: Boolean })
+  readonly visible!: boolean
+
+  get mVisible() {
+    return this.visible
+  }
+
+  set mVisible(val: boolean) {
+    this.$emit('input', val)
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
