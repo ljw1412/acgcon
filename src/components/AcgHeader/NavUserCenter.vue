7825c557ff37aeee73afd7ee10bb0f6139893fc0
ljw1412 <506644791@qq.com>
2020-03-27 22:44:48 +0800

    增加登录

--- src/components/AcgHeader/NavUserCenter.vue
+++ src/components/AcgHeader/NavUserCenter.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="nav-user-center">用户名</div>
+  <div class="nav-user-center"
+    @click="onUserClick">用户名</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 @Component
-export default class NavUserCenter extends Vue {}
+export default class NavUserCenter extends Vue {
+  onUserClick() {
+    this.$acg.user.login.show()
+  }
+}
 </script>
 
 <style lang="scss">
