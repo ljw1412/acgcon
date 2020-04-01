11e4f0f97c9bed632c35e568cfd76a2707b17672
ljw1412 <506644791@qq.com>
2020-04-01 18:39:03 +0800

    样式优化

--- src/views/Main/_Common/Baike/List/Content.vue
+++ src/views/Main/_Common/Baike/List/Content.vue
@@ -3,7 +3,18 @@
     <div v-for="item of list"
       class="acg-baike-content__item"
       :key="item._id">
-      <mz-card :elevation="0">123</mz-card>
+      <mz-card :elevation="0">
+        <mz-image src="https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3075428487,1084342510&fm=58&s=65F61AC6426290FC9548141A03001010"
+          height="100%"
+          width="40%"
+          fit="cover"
+          position="center"
+          class="baike-cover"></mz-image>
+        <div class="baike-info">
+          <mz-heading class="baike-info__title"
+            :level="6">标题</mz-heading>
+        </div>
+      </mz-card>
     </div>
   </div>
 </template>
@@ -34,9 +45,26 @@ export default class AcgBaikeContent extends AcgVue {
     transition: height 0.15s linear;
     will-change: width, height;
     .mz-card {
+      display: flex;
       width: 100%;
       border-radius: 4px;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.08);
+      transition: box-shadow 0.25s;
+      &:hover {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.24);
+      }
+    }
+
+    .baike-cover {
+      flex-shrink: 0;
+    }
+
+    .baike-info {
+      flex-grow: 1;
+      padding: 5px 10px;
+      &__title {
+        font-weight: 500;
+      }
     }
   }
 }
