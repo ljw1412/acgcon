5fa3c42983654bcba2713d6274867b4a224c09e2
ljw1412 <506644791@qq.com>
2020-04-12 17:18:16 +0800

    优化百科元素数据格式

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -8,6 +8,14 @@ declare global {
       type?: string
       to?: RawLocation
     }
+    type BaikeItem = {
+      _id: string | number
+      title: string
+      author: string
+      cover: string
+      desc: string
+      tags?: { name: string; color?: string; textColor?: string }[]
+    }
   }
 
   interface ResizeObserverEntry {
--- src/views/Main/_Common/Baike/List/Content.vue
+++ src/views/Main/_Common/Baike/List/Content.vue
@@ -17,7 +17,8 @@
           <div class="baike-info__tags">
             <mz-tag v-for="tag of item.tags"
               size="small"
-              :key="tag">{{tag}}</mz-tag>
+              :color="tag.color"
+              :key="tag.name">{{tag.name}}</mz-tag>
           </div>
           <mz-heading class="baike-info__author"
             :level="8">{{item.author}}</mz-heading>
@@ -35,7 +36,7 @@ import AcgVue from '@/mixins/AcgVue'
 
 @Component
 export default class AcgBaikeContent extends AcgVue {
-  list = [
+  list: Acgcon.BaikeItem[] = [
     {
       _id: 1,
       title: '关于我转生变成史莱姆这档事',
@@ -43,7 +44,7 @@ export default class AcgBaikeContent extends AcgVue {
       cover: '',
       desc:
         '史莱姆生活，开始了。上班族的三上悟在道路上被歹徒给刺杀身亡后，回过神来发现自己转生到了异世界。不过，自己居然是“史莱姆”！他在得到利姆鲁这个名字后开始了自己的史莱姆人生，随着与各个种族相处交流的过程中，他定下了一个目标——那就是“建立一个任何种族都能愉快地一起生活的国家”！',
-      tags: ['2019', '异世界']
+      tags: [{ name: '2019' }, { name: '异世界' }]
     },
     {
       _id: 2,
@@ -61,7 +62,7 @@ export default class AcgBaikeContent extends AcgVue {
       desc:
         '随着和平的到来而走向近代化的木叶隐村。高楼林立，巨大显示屏中播放出影像，连结各区域的电车在村里奔驰。虽说是忍者村，但一般民众也增多，忍者的生存方式也在逐渐改变的这个时代——村子的领袖、第七代火影·漩涡鸣人的儿子博人，进入了培育忍者的学校“忍者学校”。周围的学生们带着“火影的儿子”这样偏见的目光看待博人，但博人凭借天生的破天荒性格将这种小事轻松越过！博人与新的伙伴相遇，他将如何挑战突然发生的神秘事件？在众人心中如疾风般狂奔的“漩涡博人”的物语，现在开始！！'
     },
-    { _id: 4 }
+    { _id: 4, title: '', author: '', cover: '', desc: '' }
   ]
 }
 </script>
