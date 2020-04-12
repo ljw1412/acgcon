c20bec66e0086ebc3ee0010cc695bf9feae5cb9f
ljw1412 <506644791@qq.com>
2020-04-12 17:18:37 +0800

    增加百科顶部信息

---
+++ src/views/Main/_Common/Baike/Detail/Info.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="acg-baike-info">
+    <mz-image class="acg-baike-info-bg"
+      background
+      width="110%"
+      height="110%"
+      fit="cover"
+      :src="info.cover"></mz-image>
+    <div class="acg-baike-info-content">
+      <div class="acg-baike-info-inner flex">
+        <div class="acg-baike-info-cover">
+          <mz-image fit="cover"
+            :src="info.cover"></mz-image>
+        </div>
+        <div class="acg-baike-info-right">
+          <mz-heading class="acg-baike-info-title"
+            :level="1">{{info.title}}</mz-heading>
+          <div class="acg-baike-info-tags">
+            <mz-tag v-for="tag of info.tags"
+              size="medium"
+              outlined
+              :color="tag.color"
+              :key="tag.name">{{tag.name}}</mz-tag>
+          </div>
+          <div class="acg-baike-info-author">{{info.author}}</div>
+          <div class="acg-baike-info-desc">{{info.desc}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class AcgBaikeInfo extends Vue {
+  @Prop({ type: Object, default: () => ({}) })
+  readonly info!: Acgcon.BaikeItem
+}
+</script>
+
+<style lang="scss">
+.acg-baike-info {
+  height: 440px;
+  background-color: $black-background-color;
+  overflow: hidden;
+  transform: translateZ(0);
+}
+
+.acg-baike-info-bg {
+  position: absolute;
+  top: -5%;
+  background-repeat: no-repeat;
+  z-index: 0;
+  filter: blur(40px);
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    background-color: rgba($color: #000000, $alpha: 0.3);
+  }
+}
+
+.acg-baike-info-content {
+  position: relative;
+  max-width: 1600px;
+  height: calc(100% - 50px);
+  margin: 0 auto;
+  padding: 0 40px;
+  padding-top: 50px;
+  box-sizing: border-box;
+}
+
+.acg-baike-info-inner {
+  padding: 45px 0;
+}
+
+.acg-baike-info-cover {
+  flex-shrink: 0;
+  width: 225px;
+  height: 300px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.acg-baike-info-right {
+  flex-grow: 1;
+  margin-left: 20px;
+  color: #ffffff;
+}
+
+.acg-baike-info-title {
+  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.3);
+}
+
+.acg-baike-info-tags,
+.acg-baike-info-author,
+.acg-baike-info-desc {
+  margin-top: 10px;
+}
+
+.acg-baike-info-tags {
+  .mz-tag + .mz-tag {
+    margin-left: 5px;
+  }
+}
+
+.acg-baike-info-desc {
+  @include multi-ellipsis(7);
+}
+</style>
--- src/views/Main/_Common/Baike/Detail/index.vue
+++ src/views/Main/_Common/Baike/Detail/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="acg-baike-detail">
     <acg-navigation></acg-navigation>
-    <div class="acg-baike-info"></div>
+    <baike-info :info="info"></baike-info>
   </div>
 </template>
 
@@ -9,9 +9,21 @@
 import { Component, Vue } from 'vue-property-decorator'
 import AcgVue from '@/mixins/AcgVue'
 import AcgNavigation from '@/components/AcgNavigation/index.vue'
+import BaikeInfo from './Info.vue'
 
-@Component({ components: { AcgNavigation } })
-export default class AcgBaikeDetail extends AcgVue {}
+@Component({ components: { AcgNavigation, BaikeInfo } })
+export default class AcgBaikeDetail extends AcgVue {
+  info: Acgcon.BaikeItem = {
+    _id: 1,
+    title: '关于我转生变成史莱姆这档事',
+    author: '8bit',
+    cover:
+      'https://dss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3075428487,1084342510&fm=58&s=65F61AC6426290FC9548141A03001010',
+    desc:
+      '史莱姆生活，开始了。上班族的三上悟在道路上被歹徒给刺杀身亡后，回过神来发现自己转生到了异世界。不过，自己居然是“史莱姆”！他在得到利姆鲁这个名字后开始了自己的史莱姆人生，随着与各个种族相处交流的过程中，他定下了一个目标——那就是“建立一个任何种族都能愉快地一起生活的国家”！',
+    tags: [{ name: '2019' }, { name: '异世界' }]
+  }
+}
 </script>
 
 <style lang="scss">
@@ -20,11 +32,7 @@ export default class AcgBaikeDetail extends AcgVue {}
   > .acg-navigation {
     position: absolute;
     height: 50px;
-  }
-
-  .acg-baike-info {
-    height: 440px;
-    background-color: $black-background-color;
+    z-index: 10;
   }
 }
 </style>
