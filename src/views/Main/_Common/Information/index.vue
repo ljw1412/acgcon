b3d1e7ed02e1be263c3939b8beeed11dae8925ed
ljw1412 <506644791@qq.com>
2020-06-24 18:39:32 +0800

    feat: 基础列表显示

--- src/views/Main/_Common/Information/index.vue
+++ src/views/Main/_Common/Information/index.vue
@@ -1,11 +1,54 @@
 <template>
-  <div class="acg-information"></div>
+  <div class="acg-information">
+
+    <ul style="min-height:500px">
+      <li v-for="item of list"
+        :key="item._id">{{item.title}}</li>
+    </ul>
+    <mz-pagination v-model="index"
+      :layout="['total', '|', 'prev', 'pager', 'next']"
+      :pageSize.sync="size"
+      :total="count"></mz-pagination>
+
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import AcgVue from '@/mixins/AcgVue'
+
 @Component
-export default class AcgInformation extends Vue {}
+export default class AcgInformation extends AcgVue {
+  index = 1
+  size = 20
+  count = 0
+  list = []
+  error = false
+  loading = false
+
+  async fetchInformationList() {
+    this.error = false
+    this.loading = true
+    try {
+      const { list, count } = await this.$get('information', {
+        params: { acgType: this.acgType, index: this.index, size: this.size }
+      })
+      this.list = list
+      this.count = count
+    } catch (error) {
+      this.error = true
+    } finally {
+      this.loading = false
+    }
+  }
+
+  @Watch('index', { immediate: true })
+  async onIndexChange() {
+    this.list = []
+    await this.$nextTick()
+    this.fetchInformationList()
+  }
+}
 </script>
 
 <style lang="scss">
