deb757a0f06cfb3948c923f973995fb7179021c7
ljw1412 <506644791@qq.com>
2020-09-23 20:02:45 +0800

    chore: 新增资讯页面

---
+++ src/views/Main/Information/InfoCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <mz-card outlined
+    class="info-card">
+    <div class="info-card-cover">
+      <acg-image :src="item.cover"
+        fit="cover">
+      </acg-image>
+    </div>
+    <div class="info-card-core">
+      <h3>{{item.title}}</h3>
+      <p class="info-card-info">
+        <span>来源：{{item.originCN}}</span>
+        <span>时间：{{item.time}}</span>
+      </p>
+      <p class="info-card-desc">{{item.desc}}</p>
+    </div>
+  </mz-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class InfoCard extends Vue {
+  @Prop({ type: Object, default: () => ({}) })
+  readonly item!: object
+}
+</script>
+
+<style lang="scss">
+.info-card {
+  position: relative;
+  display: flex;
+  height: 100%;
+  border-radius: 4px;
+  transition: all 0.2s;
+  overflow: hidden;
+  box-sizing: border-box;
+  &:hover {
+    transform: translateY(-5px);
+    @include elevation(10);
+  }
+  &:active {
+    opacity: 0.6;
+  }
+
+  .info-card-cover {
+    flex: 1 0 auto;
+    width: 30%;
+    height: 100%;
+  }
+
+  .info-card-core {
+    width: 70%;
+    padding: 16px;
+  }
+
+  .info-card-info {
+    margin-top: 8px;
+    font-size: 12px;
+    line-height: 14px;
+    > span + span {
+      margin-left: 10px;
+    }
+  }
+
+  .info-card-desc {
+    $line-height: 20px;
+    $line: 3;
+    @include multi-text-truncate($line);
+    height: $line * $line-height;
+    line-height: $line-height;
+    font-size: 14px;
+    margin-top: 8px;
+  }
+}
+</style>
--- src/views/Main/Information/index.vue
+++ src/views/Main/Information/index.vue
@@ -1,14 +1,96 @@
 <template>
   <div class="acg-information">
+    <mz-loading background="transparent"
+      :loading="loading">
+      <mz-row :gutter="20"
+        tag="transition-group"
+        name="mz-zoom"
+        class="information-list"
+        style="min-height:500px;">
+        <mz-col v-for="item of list"
+          tag="a"
+          target="_blank"
+          class="information-item"
+          style="margin-bottom: 20px;"
+          :md="12"
+          :xs="24"
+          :href="item.url"
+          :key="item._id">
+          <info-card :item="item"></info-card>
+        </mz-col>
+      </mz-row>
+
+      <mz-pagination v-model="pageIndex"
+        :layout="['total', '|', 'prev', 'pager', 'next']"
+        :pageSize.sync="pageSize"
+        :total="count"></mz-pagination>
+    </mz-loading>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Watch } from 'vue-property-decorator'
+import InfoCard from './InfoCard.vue'
+import { fetchInformationList } from '@/services/information'
+import AcgVue from '@/mixins/AcgVue'
 
-@Component
-export default class AcgInformation extends Vue {}
+@Component({ components: { InfoCard } })
+export default class AcgInformation extends AcgVue {
+  pageIndex = -1
+  pageSize = 24
+  count = 0
+  list = []
+  error = false
+  loading = false
+
+  async fetchInformationList() {
+    this.error = false
+    this.loading = true
+    try {
+      const { list, count } = await fetchInformationList({
+        acgType: this.acgType,
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      })
+      list.forEach((item: any) => {
+        if (item.time) item.time = moment(item.time).format('YYYY-MM-DD HH:mm')
+      })
+      this.list = list
+      this.count = count
+    } catch (error) {
+      this.error = true
+    } finally {
+      this.loading = false
+    }
+  }
+
+  @Watch('pageIndex')
+  async onIndexChange(val: number, oldVal?: number) {
+    if (oldVal !== -1) {
+      this.list = []
+      await this.$nextTick()
+      this.$router.replace({ query: { p: this.pageIndex + '' } })
+      window.scrollTo({ top: 258, behavior: 'smooth' })
+    }
+    this.fetchInformationList()
+  }
+
+  created() {
+    const p = this.$route.query.p as string
+    this.pageIndex = p ? parseInt(p) : 1
+  }
+}
 </script>
 
 <style lang="scss">
+.acg-information {
+  margin-bottom: 20px;
+  overflow: hidden;
+  padding-top: 6px;
+
+  .information-list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
 </style>
